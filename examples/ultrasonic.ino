1a3140d43eb583f57b548bae979b66e2e3d12cd4
root <pbobbink@gmail.com>
2018-04-23 20:59:18 +0200

    first commit

---
+++ examples/ultrasonic.ino
@@ -0,0 +1,29 @@
+#include "ultrasonic.h"
+
+#define TRIG_PIN 5
+#define ECHO_PIN 6
+
+Ultrasonic us(TRIG_PIN, ECHO_PIN);
+float dt;
+int startTime;
+
+void setup() {
+
+  Serial.begin(1000000);
+  while(!Serial)
+    ;
+
+  startTime = millis();  
+}
+
+void loop() {
+
+
+  us.measure();
+  dt = millis() - startTime;
+  dt /= 1000;
+  Serial.print(dt, 3); Serial.print(" ");
+  Serial.println(us.get_cm(), 3);
+  
+  delay(2);
+}
